2b93e07560743d7d2d48323ad51d4ab75708838f
Karl Hans Laurits <karl@laurits.dev>
2026-09-08 08:13:25 +0000

    Add tailnet management and simplify

--- locals.tf
+++ locals.tf
@@ -5,35 +5,4 @@ locals {
   app_uid        = "1000"
   app_gid        = "1000"
   tailnet_domain = "cormo-tegu.ts.net"
-
-  tailscale_serve_services = {
-    "svc:zerobyte" = {
-      TCP = { "443" = { HTTPS = true } }
-      Web = { "zerobyte.${local.tailnet_domain}:443" = { Handlers = { "/" = { Proxy = "http://zerobyte:4096" } } } }
-    }
-    "svc:jellyfin" = {
-      TCP = { "443" = { HTTPS = true } }
-      Web = { "jellyfin.${local.tailnet_domain}:443" = { Handlers = { "/" = { Proxy = "http://jellyfin:8096" } } } }
-    }
-    "svc:prowlarr" = {
-      TCP = { "443" = { HTTPS = true } }
-      Web = { "prowlarr.${local.tailnet_domain}:443" = { Handlers = { "/" = { Proxy = "http://prowlarr:9696" } } } }
-    }
-    "svc:radarr" = {
-      TCP = { "443" = { HTTPS = true } }
-      Web = { "radarr.${local.tailnet_domain}:443" = { Handlers = { "/" = { Proxy = "http://radarr:7878" } } } }
-    }
-    "svc:sonarr" = {
-      TCP = { "443" = { HTTPS = true } }
-      Web = { "sonarr.${local.tailnet_domain}:443" = { Handlers = { "/" = { Proxy = "http://sonarr:8989" } } } }
-    }
-    "svc:transmission" = {
-      TCP = { "443" = { HTTPS = true } }
-      Web = { "transmission.${local.tailnet_domain}:443" = { Handlers = { "/" = { Proxy = "http://transmission:9091" } } } }
-    }
-    "svc:immich" = {
-      TCP = { "443" = { HTTPS = true } }
-      Web = { "immich.${local.tailnet_domain}:443" = { Handlers = { "/" = { Proxy = "http://immich-server:2283" } } } }
-    }
-  }
 }
